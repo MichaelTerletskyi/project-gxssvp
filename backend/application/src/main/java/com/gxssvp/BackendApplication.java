6e9301211c9164e6e046253fd7d831ee6eef3dec
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2026-07-14 17:51:35 +0300

    Checkstyle plugin for backend component has been added

---
+++ backend/application/src/main/java/com/gxssvp/BackendApplication.java
@@ -0,0 +1,16 @@
+package com.gxssvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry points of backend app.
+ *
+ * @author Michael Terletskyi
+ */
+@SpringBootApplication
+public class BackendApplication {
+    static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
